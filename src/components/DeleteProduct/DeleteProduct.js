a95556b4a24ceba0abe14a9009c290f75f93a9ea
Sultana Tasnim Jahan <tasnim047sultana@gmail.com>
2021-04-01 22:19:30 +0600

    submitting

--- src/components/DeleteProduct/DeleteProduct.js
+++ src/components/DeleteProduct/DeleteProduct.js
@@ -14,24 +14,6 @@ const DeleteProduct = ({product}) => {
     const [loggedInUser,setLoggedInUser] = useContext(UserContext);
     console.log(loggedInUser);
     
-    
-    
-    const handleOrders = () => {
-        const newOrders = {...loggedInUser, ...selectedDate};
-        fetch('https://pumpkin-shortcake-83525.herokuapp.com/addOrders', {
-            method:'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(newOrders)
-        })
-        .then(response => response.json())
-        .then(data=>{
-            console.log(data);
-        })
-    }
-    
-    
-    
-    
         const deleteEvent=(id)=>{
             // console.log(id);
             fetch(`https://pumpkin-shortcake-83525.herokuapp.com/deleteProduct/${id}`, {
@@ -43,7 +25,7 @@ const DeleteProduct = ({product}) => {
                 console.log("result ta bujhinai= result holo=", result);
                 if(result){
                     console.log("owaoo deleted");
-                    location.reload();
+                    document.getElementById("productRow").style.display="none"
                 }
             })
             
@@ -53,12 +35,12 @@ const DeleteProduct = ({product}) => {
             // <div>
     
     
-       <tbody>
+       <tbody id="productRow">
             <tr>
-            <td scope="col">{product.name}</td>
-            <td scope="col">{product.price}</td>
-            <td onClick={() =>deleteEvent(product._id)} scope="col"><img className="deleteIcon" src={DeleteIcon} alt=""/></td>
-        </tr>
+                <td scope="col">{product.name}</td>
+                <td scope="col">{product.price}</td>
+                <td onClick={() =>deleteEvent(product._id)} scope="col"><img className="deleteIcon" src={DeleteIcon} alt=""/></td>
+            </tr>
        </tbody>
                 
                    
